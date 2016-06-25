d76b903aa47b1256f210390b0911a17af06aebb6
Ethan Young <ethankyoung@gmail.com>
2016-06-25 17:02:20 -0600

    Change the test report file name.

--- fbmessenger_suite_test.go
+++ fbmessenger_suite_test.go
@@ -15,6 +15,6 @@ func TestWebhooks(t *testing.T) {
 	RegisterFailHandler(Fail)
 	testReportsPath, _ := filepath.Abs("./test-reports")
 	os.MkdirAll(testReportsPath, 0777)
-	junitReporter := reporters.NewJUnitReporter(filepath.Join(testReportsPath, "lib-webhooks-junit.xml"))
+	junitReporter := reporters.NewJUnitReporter(filepath.Join(testReportsPath, "fbmessenger-junit.xml"))
 	RunSpecsWithDefaultAndCustomReporters(t, "Webhooks Suite", []Reporter{junitReporter})
 }
